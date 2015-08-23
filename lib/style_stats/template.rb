30d847e8f0712512bf1ceff1debd2f24b47f6f43
shiro16 <nyanyanyawan24@gmail.com>
2015-08-24 01:27:01 +0900

    change template class

--- lib/style_stats/template.rb
+++ lib/style_stats/template.rb
@@ -1,9 +1,14 @@
 class StyleStats
   class Template
-    def initialize(result, options)
+    def initialize(css, options)
+      @css = css
     end
 
     def render
+      text = File.read "lib/style_stats/templates/default.erb"
+      @paths = []
+      @csses = []
+      ERB.new(text).run(binding)
     end
   end
 end
