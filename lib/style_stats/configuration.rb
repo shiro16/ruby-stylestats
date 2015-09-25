d05bb3ad70d8fb9dc8ba339bed88ad66233aa106
shiro16 <nyanyanyawan24@gmail.com>
2015-09-26 00:32:59 +0900

    update default options

--- lib/style_stats/configuration.rb
+++ lib/style_stats/configuration.rb
@@ -3,6 +3,9 @@ class StyleStats
     def options
       @options ||= {
         stylesheets:                   true,
+        paths:                         true,
+        stylesheets:                   true,
+        styleElements:                 true,
         size:                          true,
         dataUriSize:                   true,
         ratioOfDataUriSize:            true,
@@ -10,24 +13,29 @@ class StyleStats
         simplicity:                    true,
         rules:                         true,
         selectors:                     true,
+        declarations:                  true,
+        averageOfIdentifier:           true,
         mostIdentifier:                true,
         mostIdentifierSelector:        true,
+        averageOfCohesion:             true,
         lowestCohesion:                true,
         lowestCohesionSelector:        true,
         totalUniqueFontSizes:          true,
         uniqueFontSizes:               true,
-        totalUniqueColors:             true,
+        totalUniqueFontFamilies:       true,
         uniqueFontFamilies:            true,
+        totalUniqueColors:             true,
         uniqueColors:                  true,
         idSelectors:                   true,
         universalSelectors:            true,
         unqualifiedAttributeSelectors: true,
-        javascriptSpecificSelectors:   true,
-        userSpecifiedSelectors:        '#foo',
+        javascriptSpecificSelectors:   "[#\\.]js\\-",
+        userSpecifiedSelectors:        false,
         importantKeywords:             true,
         floatProperties:               true,
         mediaQueries:                  true,
-        propertiesCount:               true
+        propertiesCount:               10,
+        requestOptions:                {}
       }
     end
   end
